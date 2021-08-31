1d4003c65ab8b3d6615a7958e6ad585231a36098
johnb432 <58661205+johnb432@users.noreply.github.com>
2021-08-31 16:13:49 +0200

    more stuff

--- app/src/main/java/ch/hevs/ss1/MyKartRemote.java
+++ app/src/main/java/ch/hevs/ss1/MyKartRemote.java
@@ -1,90 +1,93 @@
 package ch.hevs.ss1;
 
+import android.content.Context;
+import android.content.pm.ActivityInfo;
+import android.hardware.Sensor;
 import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.SeekBar;
+import android.widget.Switch;
 import android.widget.TextView;
 
 import ch.hevs.kart.AbstractKartControlActivity;
 import ch.hevs.kart.Kart;
 import ch.hevs.kart.KartStatusRegister;
+import ch.hevs.kart.KartStatusRegisterListener;
 
 public class MyKartRemote extends AbstractKartControlActivity {
     //public KartSetup kartSetup = new KartSetup();
-    //public int batteryLevel = 0;
-    public int throttleLevel = 0;
-    public int steeringAngle = 0;
     public boolean lightsOn = false;
+
+    private long oldTime = 0;
+    private long newTime;
+
+    private Switch throttleToggle;
+    private Switch steeringToggle;
+
+    public TextView speedDisplay;
+    public TextView throttleDisplay;
 
     public ProgressBar batteryLevelBar;
     public ProgressBar steeringAngleBar;
+    public ProgressBar speedBarPos;
+    public ProgressBar speedBarNeg;
 
-    //public ch.hevs.kart.KartStatusRegister KartStatusRegister = new KartStatusRegister();
+    public SensorManager SensorManager;
+
+    final float alpha = 0.8f;
+    float[] gravity = new float[3];
+    float[] linear_acceleration = new float[3];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_kart_remote);
 
-        showKartSetupPopup();
-
-        //batteryLevel = (int) kart.getBatteryLevel();
-        //batteryLevel = 10;
-
-        //kart.addStatusRegisterListener(KartStatusRegisterListener k);
+        //showKartSetupPopup();
 
         //kart.setup();
-        //kart
+
+        throttleToggle = findViewById(R.id.throttleToggle);
+        steeringToggle = findViewById(R.id.steeringToggle);
 
         batteryLevelBar = findViewById(R.id.batteryLevelBar);
         steeringAngleBar = findViewById(R.id.steeringAngleBar);
+        speedBarPos = findViewById(R.id.speedBarPos);
+        speedBarNeg = findViewById(R.id.speedBarNeg);
 
+        speedDisplay = findViewById(R.id.speedDisplay);
+        throttleDisplay = findViewById(R.id.throttleDisplay);
 
-        /*
+        kart.addStatusRegisterListener(new KartStatusRegisterListener() {
+            @Override
+            public void statusRegisterHasChanged(Kart kart, KartStatusRegister kartStatusRegister, int i) {
+                switch (kartStatusRegister.name()) {
+                    case "HallSensorCounter1": {
+                        Log.d("statusRegisterHasChanged", "speedDisplay: " + i);
+                        newTime = System.nanoTime();
 
-        final TextView speedDisplay = findViewById(R.id.speedDisplay);
-        speedDisplay.setText(kart.getDriveSpeed());
+                        float speed = 0.08f / (newTime - oldTime) / 1000;
+                        speed = (int) speed * 100;
+                        speed /= 100;
 
+                        speedDisplay.setText("" + speed);
+                        oldTime = newTime;
 
-        final ProgressBar speedBarPos = findViewById(R.id.speedBarPos);
-        speedBarPos.setProgress(kart.getDriveSpeed(), true);
-
-         */
-
-        final Button resetSteering = findViewById(R.id.resetSteering);
-        resetSteering.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                // Code here executes on main thread after user presses button
-                kart.resetSteering();
-                System.out.println("Reset steering");
-            }
-        });
-
-        final Button throttleToggle = findViewById(R.id.throttleToggle);
-        throttleToggle.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                // Code here executes on main thread after user presses button
-                System.out.println("Toggled throttle control");
-            }
-        });
-
-        final Button steeringToggle = findViewById(R.id.steeringToggle);
-        steeringToggle.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                // Code here executes on main thread after user presses button
-                System.out.println("Toggled steering control");
-            }
-        });
-
-        final Button lightSwitch = findViewById(R.id.lightSwitch);
-        lightSwitch.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                // Code here executes on main thread after user presses button
-                kart.toggleLed(0);
-                System.out.println("Toggled light 0");
+                        if (i >= 0) {
+                            speedBarPos.setProgress((int) speed);
+                            speedBarNeg.setProgress(0);
+                        } else {
+                            speedBarPos.setProgress(0);
+                            speedBarNeg.setProgress((int) speed);
+                        }
+                    }
+                }
             }
         });
 
@@ -92,10 +95,7 @@ public class MyKartRemote extends AbstractKartControlActivity {
         steeringAngleSlider.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             // Code here executes on main thread after user moves slider
             public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
-                if (b) {
-                    steeringAngle = i;
-                    kart.setSteeringPosition(i);
-                }
+                kart.setSteeringPosition(i);
             }
 
             public void onStartTrackingTouch(SeekBar seekBar) {}
@@ -107,43 +107,75 @@ public class MyKartRemote extends AbstractKartControlActivity {
         throttleLevelSlider.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             // Code here executes on main thread after user moves slider
             public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
-                if (b) {
-                    throttleLevel = i;
-
-                    //if (kart.getDriveSpeed() >= i) {
-                        kart.setDriveSpeed(i);
-                        /*
-                    } else {
-                        kart.decreaseDriveSpeed();
-                    }
-                    */
-                }
+                kart.setDriveSpeed(i);
+                throttleDisplay.setText("" + i);
             }
 
             public void onStartTrackingTouch(SeekBar seekBar) {}
 
             public void onStopTrackingTouch(SeekBar seekBar) {}
         });
+
+        final Button resetSteering = findViewById(R.id.resetSteering);
+        resetSteering.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+                //kart.resetSteering();
+                kart.setSteeringPosition(300);
+                steeringAngleSlider.setProgress(300, true);
+                Log.d("resetSteering", "Reset steering");
+            }
+        });
+
+        final Button lightSwitch = findViewById(R.id.lightSwitch);
+        lightSwitch.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+                kart.toggleLed(0);
+                Log.d("lightSwitch", "Toggled light 0");
+            }
+        });
+
+        SensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        SensorManager.registerListener(new SensorEventListener() {
+            @Override
+            public final void onSensorChanged(SensorEvent event) {
+                if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+                    Log.d("Accelerometer", "Sensor Changed value:" + event.values[0] + ":" + event.values[1] + ":" + event.values[2]);
+
+                    if (throttleToggle.isChecked()) {
+                        int speed = (int) (event.values[2] * 1.75);
+                        throttleLevelSlider.setProgress(speed);
+                        //kart.setDriveSpeed(speed);
+                    }
+
+                    if (steeringToggle.isChecked()) {
+                        int angle = (int) (event.values[1]) * 30 + 300;
+                        steeringAngleSlider.setProgress(angle);
+                        //kart.setSteeringPosition(angle);
+                    }
+                }
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int i) {}
+        }, SensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL);
     }
 
     protected void onResume() {
         super.onResume();
 
-        //kart.addStatusRegisterListener();
-    }
+        //throttleAccelerometerOn = throttleToggle.isChecked();
+        //steeringAccelerometerOn = steeringToggle.isChecked();
 
-    void statusRegisterHasChanged(Kart kart, KartStatusRegister register, int newValue) {
-        switch (register.name()) {
-            case HallSensorCounter1:
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
 
-                break;
-            //case 1
-        }
+        requestFullscreen();
     }
 
     @Override
     public void steeringPositionChanged(Kart kart, int i, float v) {
-        steeringAngleBar.setProgress(i, true);
+        steeringAngleBar.setProgress(kart.getSteeringPosition(), true);
     }
 
     @Override
@@ -155,7 +187,7 @@ public class MyKartRemote extends AbstractKartControlActivity {
     }
 
     @Override
-    public void sequenceCompleted(Kart kart) { }
+    public void sequenceCompleted(Kart kart) {}
 
     @Override
     public void connectionStatusChanged(Kart kart, boolean b) {
@@ -166,25 +198,5 @@ public class MyKartRemote extends AbstractKartControlActivity {
     }
 
     @Override
-    public void message(Kart kart, String s) { }
-
-    /*
-    public void onSensorChanged(SensorEvent event)  {
-        // alpha is calculated as t / (t + dT)
-        // with t, the low-pass filter's time-constant
-        // and dT, the event delivery rate
-
-        final float alpha = 0.8f;
-        float[] gravity = new float[3];
-        float[] linear_acceleration = new float[3];
-
-        gravity[0] = alpha * gravity[0] + (1 - alpha) * event.values[0];
-        gravity[1] = alpha * gravity[1] + (1 - alpha) * event.values[1];
-        gravity[2] = alpha * gravity[2] + (1 - alpha) * event.values[2];
-
-        linear_acceleration[0] = event.values[0] - gravity[0];
-        linear_acceleration[1] = event.values[1] - gravity[1];
-        linear_acceleration[2] = event.values[2] - gravity[2];
-    }
-    */
+    public void message(Kart kart, String s) {}
 }
